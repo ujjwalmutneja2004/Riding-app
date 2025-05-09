62a3681c542410b6e6347f70ca31f1150894f9b7
ujjwalmutneja2004 <124356243+ujjwalmutneja2004@users.noreply.github.com>
2025-05-09 16:01:24 +0530

    Update 1Userlogin.jsx

--- frontend/src/pages/Userlogin.jsx
+++ frontend/src/pages/Userlogin.jsx
@@ -97,7 +97,8 @@ export const Userlogin = () => {
 
       <button
   onClick={() => {
-    window.location.replace=`${import.meta.env.VITE_BASE_URL}/auth/google`;
+    // window.location.assign=`${import.meta.env.VITE_BASE_URL}/auth/google`;
+     window.location.assign(`${import.meta.env.VITE_BASE_URL}/auth/google`);
   }}
   className="flex items-center justify-center bg-white border border-gray-300 shadow-sm rounded px-4 py-2 w-full text-lg font-medium text-gray-600 hover:bg-gray-100"
 >
