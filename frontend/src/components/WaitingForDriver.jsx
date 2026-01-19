a8e51cb639e20e4bfdc3123404fc991a7b430249
ujjwalmutneja2004 <124356243+ujjwalmutneja2004@users.noreply.github.com>
2026-01-19 14:19:34 +0530

    Change fare format to Indian Rupees and update payment method
    Updated fare display format and payment method text.

--- frontend/src/components/WaitingForDriver.jsx
+++ frontend/src/components/WaitingForDriver.jsx
@@ -51,8 +51,8 @@ const WaitingForDriver = (props) => {
             <div className='flex items-center gap-5 p-3 '>
                 <i className="ri-money-pound-box-fill"></i>
                 <div >
-                    <h3 className='text-lg font-medium'>${props.ride?.fare}</h3>
-                    <p className='text-sm mt-1 text-gray-600'>Cash</p>
+                    <h3 className='text-lg font-medium'>₹{Math.round(props.ride?.fare ?? 0)}</h3>
+                    <p className='text-sm mt-1 text-gray-600'>Cash\Card</p>
                 </div>
             </div>
 
