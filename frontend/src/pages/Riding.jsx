a336db3087ae37426782896e6108fd06f4edaf8b
ujjwalmutneja2004 <124356243+ujjwalmutneja2004@users.noreply.github.com>
2026-01-21 23:43:17 +0530

    Update vehicle images for auto and moto in Riding

--- frontend/src/pages/Riding.jsx
+++ frontend/src/pages/Riding.jsx
@@ -206,9 +206,9 @@ const Riding = () => {
  const vehicleImages = {
   car: "https://www.pngplay.com/wp-content/uploads/8/Uber-PNG-Photos.png",
   auto:
-    "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png",
+    "https://img.icons8.com/color/1200/auto-rickshaw.jpg",
   moto:
-    "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnCwXru-a8075FntgQ3foHUrsz7PXsRH4ZMTgP9MFn8A&s&ec=121507586",
 };
 
   return (
