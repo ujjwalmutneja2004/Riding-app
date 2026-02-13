63b552e9cb9e044d5b8b00517b780a555aab52ea
ujjwalmutneja2004 <124356243+ujjwalmutneja2004@users.noreply.github.com>
2026-02-13 12:41:55 +0530

    Add paymentIntentId to confirmPayment and cashPayment

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -29,13 +29,38 @@ const createPaymentIntent = async (req, res) => {
 
 
 const confirmPayment = async (req, res) => {
-    const { rideId, amount } = req.body;
+    const { rideId, amount,paymentIntentId } = req.body;
     try {
         // Find the ride and captain
         const ride = await rideModel.findById(rideId).populate('captain');
         if (!ride || !ride.captain || !ride.captain.socketId) {
             return res.status(404).json({ message: "Captain not found or not online" });
         }
+
+        
+    const paymentIntent = await stripe.paymentIntents.retrieve(
+        paymentIntentId
+  );
+
+  console.log("payment Intent",paymentIntent);
+
+//   console.log(paymentIntent.status);
+
+  if (paymentIntent.status !== "succeeded") {
+    return res.status(400).json({ message: "Payment not verified" });
+  }
+   
+   const orderId = crypto.randomUUID();
+   console.log("paymentId",paymentIntent.id)
+   
+
+         await rideModel.findByIdAndUpdate(rideId, {
+      paymentMode: "card",
+      paymentStatus: "paid",
+       orderId: orderId,
+      paymentID:paymentIntent.id,
+      
+    });
         // Emit payment-success event to captain
         sendMessageToSocketId(ride.captain.socketId, {
             event: "payment-success",
@@ -47,9 +72,37 @@ const confirmPayment = async (req, res) => {
     }
 };
 
+const cashPayment = async (req, res) => {
+  const { rideId } = req.body;
+
+  const ride = await rideModel.findById(rideId).populate("captain");
+
+
+  await rideModel.findByIdAndUpdate(rideId, {
+    paymentMode: "cash",
+    paymentStatus: "paid",
+    orderId: `CASH_ORDER_${rideId}`, // Placeholder for orderId
+    paymentId: `CASH_PAYMENT_${rideId}`, // Placeholder for paymentId
+    signature: null,
+
+
+  });
+
+  // captain ko batao
+  if (ride?.captain?.socketId) {
+    sendMessageToSocketId(ride.captain.socketId, {
+      event: "cash-payment-selected"
+    });
+  }
+
+  res.json({ success: true });
+};
+
+
 module.exports = {
     createPaymentIntent,
     confirmPayment,
+    cashPayment
 };
 
 // module.exports = {
